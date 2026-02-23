0d59e7bf0ce4ab32629fff5568c7d6341f364956
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-02-24 01:24:31 +0530

    🧪 Add Timeout-Enforced Unit Tests for Python, C, and C++ Solutions  - 🧩 Adds unittest-based test suites for Python, C, and C++ implementations  - 🔗 Ensures identical test cases across languages for consistency ✅  - ⏱️ Applies strict execution timeouts to catch inefficient logic 🚨  - 🏆 Helps validate correctness, performance, and parity between implementations ⚡

---
+++ test/c_test.py
@@ -0,0 +1,32 @@
+import unittest, json, os
+from typing import List
+from timeout_decorator import timeout
+from source.c.solution import cSolution
+
+class test_c_Solution(unittest.TestCase):
+    def setUp(self):
+        currentDirectory = os.path.dirname(os.path.abspath(__file__))
+        filePath = os.path.join(currentDirectory, 'cases/test_cases.json')
+        
+        with open(
+            filePath, 
+            mode = "r", 
+            encoding = "utf-8"
+        ) as read_file: 
+            self.__testcases = json.load(read_file)
+            self.__solution = cSolution()
+
+        return super().setUp()
+    
+    @timeout(1)
+    def test(self):
+        for testcase in self.__testcases:
+            testName: str = testcase['name']
+            height: List[int] = testcase['height']
+            expectedOutput: int  = testcase['output']
+
+            with self.subTest(testName):
+                actualOutput: int = self.__solution.c_trap(height = height)
+                self.assertEqual(actualOutput, expectedOutput)
+        
+if __name__ == '__main__': unittest.main()
